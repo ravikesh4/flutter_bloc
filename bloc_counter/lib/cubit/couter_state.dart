139a9814137d78695c5c4cf746188efb6d7f6f0b
ravikesh yadav <ravikesh@netzary.com>
2020-12-06 16:10:26 +0530

    snackbar using bloc

--- bloc_counter/lib/cubit/couter_state.dart
+++ bloc_counter/lib/cubit/couter_state.dart
@@ -5,9 +5,11 @@ part of 'couter_cubit.dart';
 
 class CounterState {
   int counterValue;
+  bool wasIncremented;
   
   CounterState({
     @required this.counterValue,
+    this.wasIncremented,
   });
 
 }
--- bloc_counter/lib/main.dart
+++ bloc_counter/lib/main.dart
@@ -38,13 +38,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,43 +45,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                    // context.bloc<CounterCubit>().decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                    // context.bloc<CounterCubit>().increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                ),
-              ],
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if(state.wasIncremented == true) {
+            Scaffold.of(context).showSnackBar(
+             SnackBar(
+               content: Text('Incremented'),
+              duration: Duration(microseconds: 2000),
+             ),
+            );
+          } else if (state.wasIncremented == false) {
+            Scaffold.of(context).showSnackBar(
+             SnackBar(content: Text('Decremented'),
+             duration: Duration(microseconds: 2000),
+             ),
+            );
+          }
+        },
+        child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  BlocBuilder<CounterCubit, CounterState>(
+                    builder: (context, state) {
+                      return Text(
+                        state.counterValue.toString(),
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () {
+                          BlocProvider.of<CounterCubit>(context).decrement();
+                          // context.bloc<CounterCubit>().decrement();
+                        },
+                        tooltip: 'Decrement',
+                        child: Icon(Icons.remove),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {
+                          BlocProvider.of<CounterCubit>(context).increment();
+                          // context.bloc<CounterCubit>().increment();
+                        },
+                        tooltip: 'Increment',
+                        child: Icon(Icons.add),
+                      ),
+                    ]
+      )   
+       ],
             )
-          ],
+        
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
